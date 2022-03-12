49ab38dd563c9a874055767ff2ee5ebdfe021ffb
banggeunho <panggeunho@gmail.com>
2022-03-12 20:43:16 +0900

    Create Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
@@ -9,33 +10,26 @@ app.use(express.json())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended : true}));
 
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+
 app.get('/api/customers', (req, res) => {
-    res.send([
-        {
-            'id': 1,
-            'image': 'https://placeimg.com/64/64/any',
-            'name': '이정맹',
-            'birthday': '971819',
-            'gender': '남자',
-            'job': '대학생'
-        },
-        {
-            'id': 2,
-            'image': 'https://placeimg.com/64/64/1',
-            'name': '기면수',
-            'birthday': '911118',
-            'gender': '여자',
-            'job': '페미니스트'
-        },
-        {
-            'id': 3,
-            'image': 'https://placeimg.com/64/64/2',
-            'name': '춘길동',
-            'birthday': '922228',
-            'gender': '남자',
-            'job': '프로게이'
-        }
-    ]);
+    connection.query(
+        "SELECT * FROM customer",
+        (err, rows, fields) => {
+            res.send(rows);
+        }              
+    );
+
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
